1378459b8778446195e4460ddccd561993f442cf
Zac <zac.minidude140@Gmail.com>
2024-09-17 09:53:14 -0600

    added sum of function inside extra module

--- ExtraModuleInClassExample/ExtraModule.vb
+++ ExtraModuleInClassExample/ExtraModule.vb
@@ -1,6 +1,8 @@
 ﻿Module ExtraModule
     Function SumOf(firstNumber, secondNumber) As Integer
-
+        Dim sum As Integer
+        sum = firstNumber + secondNumber
+        Return sum
     End Function
 
 End Module
--- ExtraModuleInClassExample/MainModule.vb
+++ ExtraModuleInClassExample/MainModule.vb
@@ -11,7 +11,10 @@ Option Strict On
 Module MainModule
 
     Sub Main()
-
+        Dim test As Integer
+        test = ExtraModule.SumOf(3, 4)
+        Console.Write(test)
+        Console.Read()
     End Sub
 
 End Module
